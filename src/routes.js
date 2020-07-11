dc92cb7ecb07f6dd81463aaf9c4289fd0aee8944
Michael <bund_michael@web.de>
2020-07-11 16:19:07 +0200

    new router

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import IE from './components/IE.vue'
 import Startpage from './components/Startpage.vue'
+import Impressum from './components/Impressum.vue'
 
 export default [
     {path: '/', component: Startpage},
     {path: '/IE', component: IE},
+    {path: '/impressum', component: Impressum},
 ]
